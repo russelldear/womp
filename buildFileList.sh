71acf786131024ae9725f9206f67bc58a19b069f
Russell Dear <russell.dear@gmail.com>
2020-11-12 23:18:35 +1300

    Actually add files

---
+++ buildFileList.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd public/images
+> imageManifest.txt
+
+for filename in *.jpg; do
+    $filename >> imageManifest.txt
+    identify -format '%w %h' $filename
+done    
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface IPhoto {
+  src: string;
+  width: number;
+  height: number;
+}
+
+export interface IImage {
+  source: string;
+  caption: string;
+}
