65d75520bdc975c982aa0cfa54bcb8350ca0729d
Russell Dear <russell.dear@gmail.com>
2020-11-12 23:23:13 +1300

    Filename as string

--- buildFileList.sh
+++ buildFileList.sh
@@ -3,6 +3,6 @@ cd public/images
 > imageManifest.txt
 
 for filename in *.jpg; do
-    $filename >> imageManifest.txt
+    "$filename" >> imageManifest.txt
     identify -format '%w %h' $filename
 done    
