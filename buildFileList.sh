dea39604333dcb424b6a4d9fbd98b6059154c2e7
Russell Dear <russell.dear@gmail.com>
2020-11-22 21:30:46 +1300

    Build script for directories

--- buildFileList.sh
+++ buildFileList.sh
@@ -2,8 +2,14 @@
 cd public/images
 > imageManifest.txt
 
-for filename in *.jpg; do
-    DIMS=$(identify -format '%w %h' $filename)
-    echo "$filename $DIMS"
-    echo "$filename $DIMS" >> imageManifest.txt
-done    
+for folder in */ ; do
+    echo "$folder" >> imageManifest.txt
+    pushd $folder
+    > imageManifest.txt 
+    for filename in *.jpg; do
+        DIMS=$(identify -format '%w %h' $filename)
+        echo "$filename $DIMS"
+        echo "$filename $DIMS" >> imageManifest.txt
+    done
+    popd
+done
